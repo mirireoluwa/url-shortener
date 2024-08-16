7780eaa20753cf783efd70b38f559284555a19f6
Olalekan Oke <khallekan@Olalekans-MacBook-Air-2.local>
2024-08-16 17:08:47 +0100

    chore: review project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://cleanuri.com',
+      "/api": {
+        target: "https://cleanuri.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
+  plugins: [react()],
 });
